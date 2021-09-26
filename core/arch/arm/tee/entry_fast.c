8424503a2a3c8fc15b82fb2ad81c5d3c604a3c41
Hyeseon Oh <okw1003@gmail.com>
2021-09-26 10:45:50 +0900

    dummied out memory allocation to see memory addresses

--- core/arch/arm/tee/entry_fast.c
+++ core/arch/arm/tee/entry_fast.c
@@ -178,7 +178,8 @@ static void tee_entry_vm_destroyed(struct thread_smc_args *args)
 static void tee_entry_gpu_map_memory (struct thread_smc_args *args)
 {
 	TEE_Result ret;
-	ret = core_mmu_map_contiguous_pages((vaddr_t)(args->a1), (paddr_t)(args->a1), 1, MEM_AREA_TEE_RAM_RW);
+	ret = 0;
+	//ret = core_mmu_map_contiguous_pages((vaddr_t)(args->a1), (paddr_t)(args->a1), 1, MEM_AREA_IO_SEC);
 	args->a0 = ret;
 }
 
